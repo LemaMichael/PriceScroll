8454c47e77c02fdc452847e52fc77e6c90f6a693
Michael Lema <mel526@nyu.edu>
2018-09-07 01:04:10 -0400

    Fixing Collection view

--- Price Alert/ViewController.swift
+++ Price Alert/ViewController.swift
@@ -12,16 +12,18 @@ class ViewController: UIViewController {
     let cellID = "cellID"
     let headerID = "header"
     let footerID = "foorter"
-    let currentPrice: Double = 6495
+    let currentPrice: Double = 25
+    
+    var label = UILabel()
     
     lazy var collectionView: UICollectionView = {
         let layout = UICollectionViewFlowLayout()
         layout.scrollDirection = .horizontal
+
         let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
         collectionView.backgroundColor = UIColor(red: 0, green: 0.357, blue: 0.584, alpha: 1)
         collectionView.showsHorizontalScrollIndicator = false
         collectionView.isPagingEnabled = false
-        
         collectionView.register(AlertCell.self, forCellWithReuseIdentifier: cellID)
         collectionView.register(HeaderCell.self, forSupplementaryViewOfKind: UICollectionElementKindSectionHeader, withReuseIdentifier: headerID)
         collectionView.register(FooterCell.self, forSupplementaryViewOfKind: UICollectionElementKindSectionFooter, withReuseIdentifier: footerID)
@@ -55,8 +57,11 @@ class ViewController: UIViewController {
         collectionView.dataSource = self
         collectionView.delegate = self
         //        self.collectionView.reloadData() //: Testing purposes
-//        self.collectionView.scrollToItem(at: IndexPath(item: 0, section: 0), at: .right, animated: true)
-
+        
+        label = UILabel(frame: CGRect(x: view.center.x, y: view.center.y, width: 100, height: 100))
+        
+        label.textAlignment = .center
+        view.addSubview(label)
     }
 
     override func didReceiveMemoryWarning() {
@@ -66,7 +71,8 @@ class ViewController: UIViewController {
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
         //: Set the triangle in the middle tick
-        //self.collectionView.setContentOffset(CGPoint(x: 14.5, y: 0), animated: true)
+        let totalNumberOfItems = self.collectionView.numberOfItems(inSection: 0)
+        self.collectionView.scrollToItem(at: IndexPath(item: totalNumberOfItems/2, section: 0), at: .right, animated: false)
     }
 
 }
@@ -84,14 +90,26 @@ extension ViewController: UICollectionViewDelegateFlowLayout {
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForFooterInSection section: Int) -> CGSize {
         return CGSize(width: 45, height: 79)
     }
+//    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+//        let totalCellWidth = 45 * collectionView.numberOfItems(inSection: 0)
+//        let totalSpacingWidth = 60
+//
+//        let leftInset = (collectionView.layer.frame.size.width - CGFloat(totalCellWidth + totalSpacingWidth)) / 2
+//        let rightInset = leftInset
+//
+//        return UIEdgeInsetsMake(0, leftInset, 0, rightInset)
+//    }
 }
 
 extension ViewController: UICollectionViewDataSource {
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 5
+        let intervals = currentPrice * 2 / 5
+    
+        return Int(intervals - 1) //: Subtract one since we have a footer that accounts for the last interval
     }
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellID, for: indexPath) as! AlertCell
+        cell.label.text = "\(5 + indexPath.item * 5 )"
         return cell
     }
     
@@ -111,4 +129,19 @@ extension ViewController: UICollectionViewDataSource {
             return UICollectionReusableView()
         }
     }
+}
+
+extension ViewController {
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        findCenterIndex()
+    }
+    private func findCenterIndex() {
+        let center = self.view.convert(self.collectionView.center, to: self.collectionView)
+        let index = collectionView.indexPathForItem(at: center)
+        //: Force unwrapping for now will fix later
+        let cell = collectionView.cellForItem(at: index!) as! AlertCell
+        label.text = cell.label.text
+        print(index ?? "index not found")
+    }
+    
 }
