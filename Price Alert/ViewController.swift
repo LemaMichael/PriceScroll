9a5e3298b0696d118eb0c35a5815d1c95531dabb
Michael Lema <mel526@nyu.edu>
2018-09-07 15:54:45 -0400

    Added alert button

--- Price Alert/ViewController.swift
+++ Price Alert/ViewController.swift
@@ -16,11 +16,20 @@ class ViewController: UIViewController {
     let currency = "$"
     let grayColor = UIColor(red: 0.89, green: 0.89, blue: 0.89, alpha: 1)
     let blueColor = UIColor(red: 0.024, green: 0.404, blue: 0.816, alpha: 1)
+    var scrollPrice = String()
+    
+    let priceView = PriceView()
+    let imageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = #imageLiteral(resourceName: "Triangle")
+        imageView.contentMode = .scaleAspectFit
+        return imageView
+    }()
     
     lazy var collectionView: UICollectionView = {
         let layout = UICollectionViewFlowLayout()
         layout.scrollDirection = .horizontal
-
+        
         let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
         collectionView.backgroundColor = blueColor
         collectionView.showsHorizontalScrollIndicator = false
@@ -30,23 +39,28 @@ class ViewController: UIViewController {
         collectionView.register(LastCell.self, forCellWithReuseIdentifier: lastCell)
         return collectionView
     }()
-
-    let imageView: UIImageView = {
-        let imageView = UIImageView()
-        imageView.image = #imageLiteral(resourceName: "Triangle")
-        imageView.contentMode = .scaleAspectFit
-        return imageView
+    lazy var alertButton: UIButton = {
+        let button = UIButton(type: .custom)
+        button.backgroundColor = UIColor(red: 0.255, green: 0.573, blue: 0.863, alpha: 1)
+        button.setTitle("Create Alert", for: .normal)
+        button.titleLabel?.font = UIFont(name: "Avenir-Medium", size: 16)
+        button.setTitleColor(.white, for: .normal)
+        button.titleLabel?.alpha = 0.3
+        button.addTarget(self, action: #selector(alertTapped), for: .touchUpInside)
+        button.isEnabled = false
+        return button
     }()
-    let priceView = PriceView()
+    
     lazy var middleItem: Int = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
         self.view.backgroundColor = .gray
-        self.view.addSubview(collectionView)
         self.view.addSubview(priceView)
+        self.view.addSubview(collectionView)
         collectionView.addSubview(imageView)
-
+        self.view.addSubview(alertButton)
+        
         if #available(iOS 11.0, *) {
             self.collectionView.contentInsetAdjustmentBehavior = .never
         } else {
@@ -55,8 +69,10 @@ class ViewController: UIViewController {
         imageView.anchor(top: collectionView.topAnchor, bottom: nil, left: nil, right: nil, paddingTop: -1, paddingBottom: 0, paddingLeft: 0, paddingRight: 0, width: 30, height: 15)
         imageView.centerXAnchor.constraint(equalTo: self.view.centerXAnchor, constant: 0).isActive = true
         
-        collectionView.anchor(top: nil, bottom: self.view.bottomAnchor, left: self.view.leftAnchor, right: self.view.rightAnchor, paddingTop: 0, paddingBottom: -50, paddingLeft: 0, paddingRight: 0, width: 0, height: 79)
-
+        alertButton.anchor(top: nil, bottom: self.view.bottomAnchor, left: view.leftAnchor, right: view.rightAnchor, paddingTop: 0, paddingBottom: 0, paddingLeft: 0, paddingRight: 0, width: 0, height: 73)
+        
+        collectionView.anchor(top: nil, bottom: alertButton.topAnchor, left: self.view.leftAnchor, right: self.view.rightAnchor, paddingTop: 0, paddingBottom: 0, paddingLeft: 0, paddingRight: 0, width: 0, height: 79)
+        
         collectionView.dataSource = self
         collectionView.delegate = self
         //        self.collectionView.reloadData() //: Testing purposes
@@ -70,7 +86,10 @@ class ViewController: UIViewController {
             print(self.middleItem)
             self.collectionView.scrollToItem(at: IndexPath(item: self.middleItem, section: 0), at: .centeredHorizontally, animated: false)
         }
-        
+    }
+    
+    @objc func alertTapped() {
+        print("Button tapped at value: " + scrollPrice)
     }
 }
 
@@ -91,7 +110,7 @@ extension ViewController: UICollectionViewDelegateFlowLayout {
 extension ViewController: UICollectionViewDataSource {
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         let intervals = currentPrice * 2 / 5
-    
+        
         return Int(intervals) + 1 //: Add one since need an extra cell for that accounts for the last interval
     }
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
@@ -118,6 +137,9 @@ extension ViewController {
         findCenterIndex()
     }
     fileprivate func modifyPriceView(_ validIndex: IndexPath) {
+        alertButton.titleLabel?.alpha = (self.middleItem == validIndex[1]) ? 0.3 : 1.0
+        alertButton.isEnabled = (self.middleItem == validIndex[1]) ? false : true
+        
         priceView.belowLabel.textColor = (self.middleItem > validIndex[1]) ? self.blueColor : self.grayColor
         priceView.divider.backgroundColor = (self.middleItem > validIndex[1]) ? self.blueColor : self.grayColor
         priceView.aboveLabel.textColor = (self.middleItem < validIndex[1]) ? self.blueColor : self.grayColor
@@ -134,11 +156,15 @@ extension ViewController {
         case 0:
             let cell = collectionView.dequeueReusableCell(withReuseIdentifier: firstCell, for: validIndex) as! FirstCell
             priceView.priceLabel.text =  currency + (cell.label.text ?? "")
+            scrollPrice = priceView.priceLabel.text ?? ""
         case self.collectionView.numberOfItems(inSection: 0) - 1:
             priceView.priceLabel.text = currency + String(format: "%.0f", currentPrice * 2)
+            scrollPrice = priceView.priceLabel.text ?? ""
         default:
             if let cell = collectionView.cellForItem(at: validIndex) as? AlertCell {
                 priceView.priceLabel.text = currency + (cell.label.text ?? "")
+                scrollPrice = priceView.priceLabel.text ?? ""
+                
             }
         }
     }
